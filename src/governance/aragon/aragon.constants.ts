dea215f0f55ca34559e5bee39e09cf3d539667a5
skhomuti <skhomuti@gmail.com>
2022-06-01 14:56:27 +0500

    Aragon address to lowercase

--- src/governance/aragon/aragon.constants.ts
+++ src/governance/aragon/aragon.constants.ts
@@ -5,6 +5,6 @@ export const abi = {
 };
 
 export const ARAGON_VOTING_CONTRACT_ADDRESS =
-  '0x2e59A20f205bB85a89C53f1936454680651E618e';
+  '0x2e59a20f205bb85a89c53f1936454680651e618e';
 export const ARAGON_LINK_TEMPLATE = `https://mainnet.lido.fi/#/lido-dao/${ARAGON_VOTING_CONTRACT_ADDRESS}/vote/`;
 export const ARAGON_ADDITIONAL_LINK_TEMPLATE = `https://vote.lido.fi/vote/`;
