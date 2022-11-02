c0a8b25a898de1ef37908573f922ff1a6f2316e9
Sergey Khomutinin <31664571+skhomuti@users.noreply.github.com>
2022-11-02 12:45:30 +0400

    Update database schema if it differs from expected (#21)
    * feat: Update database schema if it differs from expected
    
    * Sync database schema before fetching items

--- src/governance/notionReporter/notion.record.entity.ts
+++ src/governance/notionReporter/notion.record.entity.ts
@@ -3,116 +3,172 @@ import { formatDate } from '../governance.utils';
 import { TopicEntity } from '../topic.entity';
 
 export class NotionTypes {
-  static title(content: string | undefined) {
+  static title = {
+    schema: { title: {} },
+    value: (content: string | undefined) => {
+      return { title: [{ text: { content: content || '' } }] };
+    },
+  };
+  static select = {
+    schema: { select: {} },
+    value: (name: string | undefined) => {
+      return { select: name ? { name } : null };
+    },
+  };
+
+  static rich_text = {
+    schema: { rich_text: {} },
+    value: (content: string | undefined, annotations) => {
+      return { rich_text: [{ text: { content: content || '' }, annotations }] };
+    },
+  };
+
+  static date = {
+    schema: { date: {} },
+    value: (start: string | undefined) => {
+      return {
+        date: start ? { start } : null,
+      };
+    },
+  };
+
+  static link = {
+    schema: { url: {} },
+    value: (url: string | undefined) => {
+      return { url: url || null };
+    },
+  };
+
+  static number = {
+    schema: { number: {} },
+    value: (number: number | undefined) => {
+      return {
+        number: number !== undefined ? number : null,
+      };
+    },
+  };
+}
+
+export class NotionEntity {
+  public static readonly propertiesNames;
+  constructor(private _: any) {}
+  public static schema() {
+    return Object.fromEntries(
+      Object.keys(this.propertiesNames).map((key) => [
+        key,
+        this.propertiesNames[key].schema,
+      ]),
+    );
+  }
+  properties() {
+    return {};
+  }
+}
+
+export class NotionVoteEntity extends NotionEntity {
+  public static readonly propertiesNames = <const>{
+    Name: NotionTypes.title,
+    Source: NotionTypes.select,
+    Status: NotionTypes.select,
+    Type: NotionTypes.select,
+    Description: NotionTypes.rich_text,
+    Date: NotionTypes.date,
+    'End Date': NotionTypes.date,
+    'Execution End Date': NotionTypes.date,
+    Link: NotionTypes.link,
+    'Additional Link': NotionTypes.link,
+    'Objections Amount': NotionTypes.number,
+    'Objections Threshold': NotionTypes.number,
+    Res1: NotionTypes.number,
+    Res2: NotionTypes.number,
+    Res3: NotionTypes.number,
+    ProposalType: NotionTypes.select,
+    Discussion: NotionTypes.link,
+    'Voters number': NotionTypes.number,
+  };
+  constructor(private vote: VoteEntity) {
+    super(vote);
+  }
+
+  properties() {
     return {
-      title: [{ text: { content: content || '' } }],
-    };
-  }
-  static select(name: string | undefined) {
-    return { select: name ? { name } : null };
-  }
-
-  static rich_text(content: string | undefined, annotations) {
-    return { rich_text: [{ text: { content: content || '' }, annotations }] };
-  }
-
-  static date(start: string | undefined) {
-    return {
-      date: start ? { start } : null,
-    };
-  }
-
-  static link(url: string | undefined) {
-    return { url: url || null };
-  }
-
-  static number(number: number | undefined) {
-    return {
-      number: number !== undefined ? number : null,
+      Name: NotionVoteEntity.propertiesNames.Name.value(this.vote.name),
+      Source: NotionVoteEntity.propertiesNames.Source.value(this.vote.source),
+      Status: NotionVoteEntity.propertiesNames.Status.value(this.vote.status),
+      Type: NotionVoteEntity.propertiesNames.Type.value(this.vote.type),
+      Description: NotionVoteEntity.propertiesNames.Description.value(
+        this.vote.description,
+        {},
+      ),
+      Date: NotionVoteEntity.propertiesNames.Date.value(this.vote.startDate),
+      'End Date': NotionVoteEntity.propertiesNames['End Date'].value(
+        this.vote.endDate,
+      ),
+      'Execution End Date': NotionVoteEntity.propertiesNames[
+        'Execution End Date'
+      ].value(this.vote.executionEndDate),
+      Link: NotionVoteEntity.propertiesNames.Link.value(this.vote.link),
+      'Additional Link': NotionVoteEntity.propertiesNames[
+        'Additional Link'
+      ].value(this.vote.additionalLink),
+      'Objections Amount': NotionVoteEntity.propertiesNames[
+        'Objections Amount'
+      ].value(this.vote.objectionsAmount),
+      'Objections Threshold': NotionVoteEntity.propertiesNames[
+        'Objections Threshold'
+      ].value(this.vote.objectionsThreshold),
+      Res1: NotionVoteEntity.propertiesNames.Res1.value(this.vote.result1),
+      Res2: NotionVoteEntity.propertiesNames.Res2.value(this.vote.result2),
+      Res3: NotionVoteEntity.propertiesNames.Res3.value(this.vote.result3),
+      ProposalType: NotionVoteEntity.propertiesNames.ProposalType.value(
+        this.vote.proposalType,
+      ),
+      Discussion: NotionVoteEntity.propertiesNames.Discussion.value(
+        this.vote.discussion,
+      ),
+      'Voters number': NotionVoteEntity.propertiesNames['Voters number'].value(
+        this.vote.votersNumber,
+      ),
     };
   }
 }
 
-export class NotionVoteEntity {
-  public static readonly propertiesNames = <const>[
-    'Name',
-    'Source',
-    'Status',
-    'Type',
-    'Description',
-    'Date',
-    'End Date',
-    'Execution End Date',
-    'Link',
-    'Additional Link',
-    'Objections Amount',
-    'Objections Threshold',
-    'Res1',
-    'Res2',
-    'Res3',
-    'ProposalType',
-    'Discussion',
-    'Voters number',
-  ];
-  constructor(private vote: VoteEntity) {}
+export class NotionTopicEntity extends NotionEntity {
+  public static readonly propertiesNames = <const>{
+    ID: NotionTypes.number,
+    Name: NotionTypes.title,
+    Link: NotionTypes.link,
+    'Creation Date': NotionTypes.date,
+    'Last Reply Date': NotionTypes.date,
+  };
 
-  properties(): {
-    [key in typeof NotionVoteEntity.propertiesNames[number]];
-  } {
+  constructor(private topic: TopicEntity) {
+    super(topic);
+  }
+
+  properties() {
     return {
-      Name: NotionTypes.title(this.vote.name),
-      Source: NotionTypes.select(this.vote.source),
-      Status: NotionTypes.select(this.vote.status),
-      Type: NotionTypes.select(this.vote.type),
-      Description: NotionTypes.rich_text(this.vote.description, {}),
-      Date: NotionTypes.date(this.vote.startDate),
-      'End Date': NotionTypes.date(this.vote.endDate),
-      'Execution End Date': NotionTypes.date(this.vote.executionEndDate),
-      Link: NotionTypes.link(this.vote.link),
-      'Additional Link': NotionTypes.link(this.vote.additionalLink),
-      'Objections Amount': NotionTypes.number(this.vote.objectionsAmount),
-      'Objections Threshold': NotionTypes.number(this.vote.objectionsThreshold),
-      Res1: NotionTypes.number(this.vote.result1),
-      Res2: NotionTypes.number(this.vote.result2),
-      Res3: NotionTypes.number(this.vote.result3),
-      ProposalType: NotionTypes.select(this.vote.proposalType),
-      Discussion: NotionTypes.link(this.vote.discussion),
-      'Voters number': NotionTypes.number(this.vote.votersNumber),
+      ID: NotionTopicEntity.propertiesNames.ID.value(this.topic.id),
+      Name: NotionTopicEntity.propertiesNames.Name.value(this.topic.name),
+      Link: NotionTopicEntity.propertiesNames.Link.value(this.topic.link),
+      'Creation Date': NotionTopicEntity.propertiesNames['Creation Date'].value(
+        this.topic.creationDate,
+      ),
+      'Last Reply Date': NotionTopicEntity.propertiesNames[
+        'Last Reply Date'
+      ].value(this.topic.lastReplyDate),
     };
   }
 }
 
-export class NotionTopicEntity {
-  public static readonly propertiesNames = <const>[
-    'ID',
-    'Name',
-    'Link',
-    'Creation Date',
-    'Last Reply Date',
-  ];
-
-  constructor(private topic: TopicEntity) {}
-
-  properties(): {
-    [key in typeof NotionTopicEntity.propertiesNames[number]];
-  } {
-    return {
-      ID: NotionTypes.number(this.topic.id),
-      Name: NotionTypes.title(this.topic.name),
-      Link: NotionTypes.link(this.topic.link),
-      'Creation Date': NotionTypes.date(this.topic.creationDate),
-      'Last Reply Date': NotionTypes.date(this.topic.lastReplyDate),
-    };
-  }
-}
-
-export function isValidProperties(validProperties, properties) {
-  return validProperties.every((name) => name in properties);
+export function isValidProperties(
+  validProperties: Record<string, unknown>,
+  properties,
+) {
+  return Object.keys(validProperties).every((name) => name in properties);
 }
 
 export function voteFromNotionProperties(properties): VoteEntity {
-  if (!isValidProperties(NotionVoteEntity.propertiesNames, properties))
-    throw Error('Notion page has invalid properties');
   return {
     source: properties.Source.select.name,
     status: properties.Status.select.name,
@@ -138,8 +194,6 @@ export function voteFromNotionProperties(properties): VoteEntity {
 }
 
 export function topicFromNotionProperties(properties): TopicEntity {
-  if (!isValidProperties(NotionTopicEntity.propertiesNames, properties))
-    throw Error('Notion page has invalid properties');
   return {
     id: properties.ID.number,
     name: properties.Name.title[0].plain_text,
